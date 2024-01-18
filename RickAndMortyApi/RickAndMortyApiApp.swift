b761a30a271f22dba51d587227dc38cb782f6c1c
Dmytro Khriapin <dmytro.khriapin@inverid.com>
2024-01-18 20:03:44 +0100

    Initial Commit

---
+++ RickAndMortyApi/RickAndMortyApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApiApp.swift
+//  RickAndMortyApi
+//
+//  Created by Dmytro Khriapin on 18/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
